e40fe833109d7f04e4e5c9fd9d2d68103e4cf11e
Bandi Stack <95058946+bandistack@users.noreply.github.com>
2026-01-30 20:06:15 -0500

    Working on navbar component from figma to vite + vue

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import '../assets/navbar.css'
+import '../assets/color.css'
+import '../assets/typography.css'
+</script>
+<template>
+  <nav class="navbar">
+    <div class="logo">Firat Frontend</div>
+    <ul class="nav-svg">
+        <li>
+
+        </li>
+    </ul>
+  </nav>
+</template>
+<style scoped>
+.navbar{
+  width: 100%;   
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: var(--navbar-height-L);
+  padding: var(--navbar-padding-L-y) var(--navbar-padding-L-x);
+  background-color: var(--color-primary);
+  box-sizing: border-box;
+  border-bottom: 0.3px solid var(--color-base);
+}
+.logo{
+  font-size: var(--font-size-title-l);
+  font-weight: var(--font-weight-title);
+  color: var(--color-text);
+}
+.nav-svg{
+  display: flex;
+  justify-content: space-between;
+}
+</style>
