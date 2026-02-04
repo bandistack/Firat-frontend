6f5c0010ed68c36bf603f2a76750b8dc23bd7e7f
Bandi Stack <95058946+bandistack@users.noreply.github.com>
2026-02-04 16:16:09 -0500

    Page logo and navbar logo updated

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,7 +1,6 @@
 <template>
-  <nav class="w-full flex justify-center h-auto px-6 py-4 bg-slate-50 shadow-md">
-    <div class="text-slate-700 hover:text-slate-400">
-      Firat Frontend
-    </div>
+  <nav class="w-full flex justify-between items-center h-auto px-6 py-4 bg-slate-50">
+    <img src="../assets/bandistack.svg" alt="Brand Logo" class="w-8"/>
+    <span class="text-slate-700 hover:text-slate-400">Firat Frontend</span>
   </nav>
 </template>
