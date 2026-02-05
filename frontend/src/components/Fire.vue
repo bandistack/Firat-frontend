f7d9b41b6c03303218e11c831e61ab8a5cb90628
Bandi Stack <95058946+bandistack@users.noreply.github.com>
2026-02-05 13:06:01 -0500

    Fixed ui consistency Boreal

--- frontend/src/components/Fire.vue
+++ frontend/src/components/Fire.vue
@@ -3,7 +3,7 @@ import Button from './Button.vue'
 import Table from './Table.vue'
 </script>
 <template>
-  <div class="flex flex-col justify-center h-auto m-6 px-1 py-1 bg-slate-50 rounded-lg">
+  <div class="flex flex-col items-center w-auto h-screen m-6 p-1 bg-slate-50 rounded-sm">
     <Button/>
     <Table/>
   </div>
--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,6 +1,5 @@
 <template>
-  <nav class="w-full flex justify-between items-center h-auto px-6 py-4 bg-slate-50">
-    <img src="../assets/bandistack.svg" alt="Brand Logo" class="w-8"/>
-    <span class="text-slate-700 hover:text-slate-400">Firat Frontend</span>
+  <nav class="w-full flex justify-between items-center h-auto px-6 py-3 bg-slate-50">
+    <span class="text-slate-700 hover:text-slate-400">Boreal</span>
   </nav>
 </template>
